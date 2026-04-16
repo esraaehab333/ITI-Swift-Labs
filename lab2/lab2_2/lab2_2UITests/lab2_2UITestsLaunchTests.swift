437e96e079e4e98c49278b81af3cf410c25f7b42
Nemo <nemo@Nemos-MacBook-Pro.local>
2026-04-16 08:30:14 +0200

    initial lab2_1

---
+++ lab2/lab2_2/lab2_2UITests/lab2_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lab2_2UITestsLaunchTests.swift
+//  lab2_2UITests
+//
+//  Created by Nemo on 16/04/2026.
+//
+
+import XCTest
+
+final class lab2_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
