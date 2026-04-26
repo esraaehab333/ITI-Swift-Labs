80f7781114183d68d3e02b0c489c251c20d01173
esraaehab333 <esraaehabb333@gmail.com>
2026-04-26 22:00:37 +0200

    lab8: images with constrains

--- lab1/lab1_1.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-// factorial function
-func getFactorial(number:Int) -> Int{
-    var ans = 1
-    for index in 1...number{
-        ans = ans * index
-    }
-    return ans
-}
-print("factorial number 5 is \(getFactorial(number: 5))")
---
+++ lab8_1/lab8_1UITests/lab8_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lab8_1UITestsLaunchTests.swift
+//  lab8_1UITests
+//
+//  Created by Nemo on 26/04/2026.
+//
+
+import XCTest
+
+final class lab8_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
