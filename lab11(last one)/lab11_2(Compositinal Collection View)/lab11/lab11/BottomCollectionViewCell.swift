296a4ce5f29a921eab53ad03ebcbf563aa0698f9
esraaehab333 <esraaehabb333@gmail.com>
2026-05-01 22:47:22 +0200

    lab 11 notification and collection view done

---
+++ lab11(last one)/lab11_2(Compositinal Collection View)/lab11/lab11/BottomCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BottomCollectionViewCell.swift
+//  lab11
+//
+//  Created by Nemo on 01/05/2026.
+//
+
+import UIKit
+
+class BottomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myLable1: UILabel!
+    @IBOutlet weak var myLable2: UILabel!
+
+}
